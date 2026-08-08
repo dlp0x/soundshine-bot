b2a25b679605697a644bdd48a55db8eff6d18f5d
n3m01726 <kiocubic.rulezz@proton.me>
2026-08-08 14:14:19 -0400

    chore: supprimer des fichiers obsolètes et nettoyer le code

--- src/api/middlewares/auth.js
+++
@@ -1,7 +0,0 @@
-// src/api/middlewares/auth.js  (NOUVEAU fichier)
-export function requireApiToken (req, res, next) {
-  if (req.headers['x-api-key'] !== process.env.ADMIN_API_KEY) {
-    return res.status(403).json({ error: 'Forbidden' });
-  }
-  next();
-}
--- src/shared/utils/generateErrorId.js
+++
@@ -1,7 +0,0 @@
-// src/utils/shared/generateErrorId.js  (NOUVEAU fichier)
-export function generateErrorId() {
-    return (
-      Math.random().toString(36).substring(2, 15)
-      + Math.random().toString(36).substring(2, 15)
-    );
-  }
